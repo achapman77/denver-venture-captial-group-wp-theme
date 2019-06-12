d7e1be2b73f83ca9fe264fc0ccf3c6eb3c0ccbd2
AChapman <achapman77@gmail.com>
2019-06-12 15:51:12 -0600

    Hooked up About Section

--- page-home.php
+++ page-home.php
@@ -14,6 +14,12 @@
     $lander_btn_2_text      =   get_field('lander_btn_2_text');
     $lander_btn_2_link      =   get_field('lander_btn_2_link');
 
+    //About Section Variables
+    $about_section_intro      =   get_field('about_section_intro');
+    $about_section_title        =   get_field('about_section_title');
+    $about_section_description  =   get_field('about_section_description');
+
+    
 
     get_header();
 ?>
@@ -83,38 +89,28 @@
 
         <div class="row section-header has-bottom-sep" data-aos="fade-up">
             <div class="col-full">
-                <h3 class="subhead subhead--dark">Hello There</h3>
-                <h1 class="display-1 display-1--light">We Are Denver Venture Capital Group</h1>
+                <h3 class="subhead subhead--dark"><?php echo $about_section_intro; ?></h3>
+                <h1 class="display-1 display-1--light"><?php echo $about_section_title; ?></h1>
             </div>
         </div> <!-- end section-header -->
 
         <div class="row about-desc" data-aos="fade-up">
             <div class="col-full">
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt. 
-                </p>
+                <?php echo $about_section_description; ?>
             </div>
         </div> <!-- end about-desc -->
 
         <div class="row about-stats stats block-1-4 block-m-1-2 block-mob-full" data-aos="fade-up">
-                
-            <div class="col-block stats__col ">
-                <div class="stats__count">100</div>
-                <h5>Dollars Invested</h5>
-            </div>
-            <div class="col-block stats__col">
-                <div class="stats__count">100</div>
-                <h5>Projects in the Pipeline</h5>
-            </div>
-            <div class="col-block stats__col">
-                <div class="stats__count">100</div>
-                <h5>Projects Completed</h5>
-            </div>
-            <div class="col-block stats__col">
-                <div class="stats__count">100</div>
-                <h5>Happy Partners</h5> 
-            </div>
 
+            <?php $loop = new WP_Query( array( 'post_type' => 'about_section_stat', 'orderby' => 'post_id', 'order' => 'ASC' ) ); ?>   
+            
+            <?php while( $loop->have_posts()): $loop->the_post(); ?>
+                <div class="col-block stats__col ">
+                    <div class="stats__count"><?php the_field('stat_value'); ?></div>
+                    <h5><?php the_title(); ?></h5>
+                </div>
+            <?php endwhile; ?>
+            
         </div> <!-- end about-stats -->
 
         <div class="about__line"></div>
