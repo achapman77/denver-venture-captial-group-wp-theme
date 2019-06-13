8f72d892f361308f3959cdecf212ee5340843533
AChapman <achapman77@gmail.com>
2019-06-12 22:50:35 -0600

    Started hooking up projects loop

--- page-home.php
+++ page-home.php
@@ -6,13 +6,13 @@
     // Header Variables
 
     //Lander Section Variables
-    $lander_feature_image   =   get_field('lander_feature_image');
-    $lander_title           =   get_field('lander_title');
-    $lander_description     =   get_field('lander_description');
-    $lander_btn_1_text      =   get_field('lander_btn_1_text');
-    $lander_btn_1_link      =   get_field('lander_btn_1_link');
-    $lander_btn_2_text      =   get_field('lander_btn_2_text');
-    $lander_btn_2_link      =   get_field('lander_btn_2_link');
+    $lander_feature_image       =   get_field('lander_feature_image');
+    $lander_title               =   get_field('lander_title');
+    $lander_description         =   get_field('lander_description');
+    $lander_btn_1_text          =   get_field('lander_btn_1_text');
+    $lander_btn_1_link          =   get_field('lander_btn_1_link');
+    $lander_btn_2_text          =   get_field('lander_btn_2_text');
+    $lander_btn_2_link          =   get_field('lander_btn_2_link');
 
     //About Section Variables
     $about_section_intro        =   get_field('about_section_intro');
@@ -20,8 +20,12 @@
     $about_section_description  =   get_field('about_section_description');
 
     //Services Section Variables
-    $services_intro     =   get_field('services_intro');
-    $services_title     =   get_field('services_title');
+    $services_section_intro     =   get_field('services_section_intro');
+    $services_section_title     =   get_field('services_section_title');
+
+    //Projects Section Variables
+    $projects_section_title     =   get_field('projects_section_title');
+    $projects_section_intro     =   get_field('projects_section_intro');
 
     get_header();
 ?>
@@ -126,8 +130,8 @@
 
         <div class="row section-header has-bottom-sep" data-aos="fade-up">
             <div class="col-full">
-                <h3 class="subhead">What We Do</h3>
-                <h1 class="display-2">We’ve got everything you need to launch and grow your business</h1>
+                <h3 class="subhead"><?php echo $services_section_intro; ?></h3>
+                <h1 class="display-2"><?php echo $services_section_title; ?></h1>
             </div>
         </div> <!-- end section-header -->
 
@@ -153,14 +157,14 @@
 
     <!-- works
     ================================================== -->
-    <section id='works' class="s-works">
+    <section id='projects' class="s-works">
 
         <div class="intro-wrap">
                 
             <div class="row section-header has-bottom-sep light-sep" data-aos="fade-up">
                 <div class="col-full">
-                    <h3 class="subhead">Recent Works</h3>
-                    <h1 class="display-2 display-2--light">We love what we do, check out some of our latest works</h1>
+                    <h3 class="subhead"><?php echo $projects_section_intro; ?></h3>
+                    <h1 class="display-2 display-2--light"><?php echo $projects_section_title; ?></h1>
                 </div>
             </div> <!-- end section-header -->
 
@@ -169,7 +173,41 @@
         <div class="row works-content">
             <div class="col-full masonry-wrap">
                 <div class="masonry">
-    
+                    <?php $loop = new WP_Query( array( 'post_type' => 'projects', 'orderby' => 'post_id', 'order' => 'ASC' ) ); ?>
+                    
+                    <?php while( $loop->have_posts()): $loop->the_post(); ?>
+                        <div class="masonry__brick" data-aos="fade-up">
+                            <div class="item-folio">
+                                    
+                                <div class="item-folio__thumb">
+                                    <a href="<?php the_field("project_image_wide"); ?>" class="thumb-link" title="<?php the_title(); ?>" data-size="1050x700">
+                                        <img src="<?php the_field("project_image_medium_square"); ?>" 
+                                            srcset="<?php the_field("project_image_medium_square"); ?> 1x, 
+                                            <?php the_field("project_image_large_square"); ?> 2x" alt="<?php the_title(); ?>">
+                                    </a>
+                                </div>
+        
+                                <div class="item-folio__text">
+                                    <h3 class="item-folio__title">
+                                        <?php the_title(); ?>
+                                    </h3>
+                                    <p class="item-folio__cat">
+                                        <?php the_field('project_category'); ?>
+                                    </p>
+                                </div>
+        
+                                <a href="<?php the_field('project_link'); ?>" class="item-folio__project-link" title="<?php the_title(); ?> link">
+                                    <i class="icon-link"></i>
+                                </a>
+        
+                                <div class="item-folio__caption">
+                                    <?php the_field('project_description'); ?>
+                                </div>
+        
+                            </div>
+                        </div> <!-- end masonry__brick -->
+                    <?php endwhile; ?>
+                    
                     <div class="masonry__brick" data-aos="fade-up">
                         <div class="item-folio">
                                 
