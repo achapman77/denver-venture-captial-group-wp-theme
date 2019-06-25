44e22e56a5a22e775c4f3abe30341344b67d3381
AChapman <achapman77@gmail.com>
2019-06-25 13:19:25 -0600

    refactored footer and hooked up contact page

--- footer.php
+++ footer.php
@@ -8,13 +8,68 @@
  *
  * @package dvcg-wp-theme
  */
-
+	//Footer Variables
+	$footer_mission_statement   =   get_field('footer_mission_statement');
+	$footer_subscribe_title     =   get_field('footer_subscribe_title');
+	$footer_subscribe_body      =   get_field('footer_subscribe_body');
 ?>
 
 	</div><!-- #content -->
 
 <!-- Footer -->
+<footer>
+        <div class="row footer-main">
 
+            <div class="col-six tab-full left footer-desc">
+
+                <div class="footer-logo"></div>
+                <p><?php echo $footer_mission_statement; ?></p>
+
+            </div>
+
+            <div class="col-six tab-full right footer-subscribe">
+
+                <h4><?php echo $footer_subscribe_title; ?></h4>
+                <p><?php echo $footer_subscribe_body; ?></p>
+
+                <div class="subscribe-form">
+                    <form id="mc-form" class="group" novalidate="true">
+                        <input type="email" value="" name="EMAIL" class="email" id="mc-email" placeholder="Email Address" required="">
+                        <input type="submit" name="subscribe" value="Subscribe">
+                        <label for="mc-email" class="subscribe-message"></label>
+                    </form>
+                </div>
+
+            </div>
+
+        </div> <!-- end footer-main -->
+
+        <div class="row footer-bottom">
+
+            <div class="col-twelve">
+                <div class="copyright">
+                    <!-- <span>© Copyright Glint 2017</span>  -->
+                    <!-- <span>Site Template by <a href="https://www.colorlib.com/">Colorlib</a></span>	 -->
+                </div>
+
+                <div class="go-top">
+                    <a class="smoothscroll" title="Back to Top" href="#top"><i class="icon-arrow-up" aria-hidden="true"></i></a>
+                </div>
+            </div>
+
+        </div> <!-- end footer-bottom -->
+
+    </footer> <!-- end footer -->
+
+
+    <!-- photoswipe background
+    ================================================== -->
+    <?php get_template_part( 'template-parts/content', 'photoswipe' ); ?>
+
+
+    <!-- preloader
+    ================================================== -->
+    <?php get_template_part( 'template-parts/content', 'preloader' ); ?>
 
 
     
--- page-home.php
+++ page-home.php
@@ -1,103 +1,42 @@
 <?php
     /* Template Name: Home Page */
     
-    //Footer Variables
-$footer_mission_statement   =   get_field('footer_mission_statement');
-$footer_subscribe_title     =   get_field('footer_subscribe_title');
-$footer_subscribe_body      =   get_field('footer_subscribe_body');
+    
 
     get_header();
 ?>
 
-    <!-- Lander
-    ================================================== -->
-    <?php get_template_part( 'template-parts/content', 'lander' ); ?>
-    
-
-    <!-- about
-    ================================================== -->
-    <?php get_template_part( 'template-parts/content', 'about' ); ?>
+<!-- Lander
+================================================== -->
+<?php get_template_part( 'template-parts/content', 'lander' ); ?>
 
 
-    <!-- services
-    ================================================== -->
-    <?php get_template_part( 'template-parts/content', 'services' ); ?>
+<!-- about
+================================================== -->
+<?php get_template_part( 'template-parts/content', 'about' ); ?>
 
 
-    <!-- works
-    ================================================== -->
-    <?php get_template_part( 'template-parts/content', 'projects' ); ?>
-    
-
-    <!-- clients
-    ================================================== -->
-    <?php get_template_part( 'template-parts/content', 'clients' ); ?>
-    
-
-    <!-- contact
-    ================================================== -->
-    <?php get_template_part( 'template-parts/content', 'contact' ); ?>
+<!-- services
+================================================== -->
+<?php get_template_part( 'template-parts/content', 'services' ); ?>
 
 
-    <!-- footer
-    ================================================== -->
-    <footer>
-        <div class="row footer-main">
-
-            <div class="col-six tab-full left footer-desc">
-
-                <div class="footer-logo"></div>
-                <p><?php echo $footer_mission_statement; ?></p>
-
-            </div>
-
-            <div class="col-six tab-full right footer-subscribe">
-
-                <h4><?php echo $footer_subscribe_title; ?></h4>
-                <p><?php echo $footer_subscribe_body; ?></p>
-
-                <div class="subscribe-form">
-                    <form id="mc-form" class="group" novalidate="true">
-                        <input type="email" value="" name="EMAIL" class="email" id="mc-email" placeholder="Email Address" required="">
-                        <input type="submit" name="subscribe" value="Subscribe">
-                        <label for="mc-email" class="subscribe-message"></label>
-                    </form>
-                </div>
-
-            </div>
-
-        </div> <!-- end footer-main -->
-
-        <div class="row footer-bottom">
-
-            <div class="col-twelve">
-                <div class="copyright">
-                    <!-- <span>© Copyright Glint 2017</span>  -->
-                    <!-- <span>Site Template by <a href="https://www.colorlib.com/">Colorlib</a></span>	 -->
-                </div>
-
-                <div class="go-top">
-                    <a class="smoothscroll" title="Back to Top" href="#top"><i class="icon-arrow-up" aria-hidden="true"></i></a>
-                </div>
-            </div>
-
-        </div> <!-- end footer-bottom -->
-
-    </footer> <!-- end footer -->
+<!-- works
+================================================== -->
+<?php get_template_part( 'template-parts/content', 'projects' ); ?>
 
 
-    <!-- photoswipe background
-    ================================================== -->
-    <?php get_template_part( 'template-parts/content', 'photoswipe' ); ?>
+<!-- clients
+================================================== -->
+<?php get_template_part( 'template-parts/content', 'clients' ); ?>
 
 
-    <!-- preloader
-    ================================================== -->
-    <?php get_template_part( 'template-parts/content', 'preloader' ); ?>
+<!-- contact
+================================================== -->
+<?php get_template_part( 'template-parts/content', 'contact' ); ?>
 
 
-
-
-
+<!-- footer
+================================================== -->
 <?php get_footer(); ?>
 
